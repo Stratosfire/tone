91dc2842e00311c74533078fc331a1b37a4ff4a8
Ashley <none>
2024-05-04 22:43:14 +0100

    make the convert all script more talkative

--- bin/convert_all_to_mid.py
+++ bin/convert_all_to_mid.py
@@ -7,6 +7,19 @@ if __name__ == '__main__':
     trackDataDict = json.loads(Path("../res/tracks.js").read_text().replace("var tracks = ", ""))
 
     mkdir("converted_midi")
+    
+    successCount = 0
+    failedArray = []
 
     for trackName, trackData in trackDataDict.items():
-        t2m.trackToMidi(trackData).save(f"converted_midi/{trackName}.mid")
+        try:
+            t2m.trackToMidi(trackData).save(f"converted_midi/{trackName}.mid")
+            print(f"Successfully converted {trackName}")
+            successCount += 1
+        except Exception as e:
+            print(f"Skipping {trackName}")
+            print(e)
+            failedArray.append(trackName)
+            
+    print(f"Successfully converted {successCount} of {len(trackDataDict.items())} tracks")
+    print(f"Failed tracks:\n\t{'\n\t'.join(failedArray)}")
